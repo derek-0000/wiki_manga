8bdd1b0f2593b3b6f8d689f72c1801fa350bcb18
Omar Ramos <omargrr21@gmail.com>
2023-03-16 13:36:13 -0600

    manga-info basic structure

--- src/app/manga-info/manga-info.page.ts
+++ src/app/manga-info/manga-info.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-manga-info',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MangaInfoPage implements OnInit {
 
+  id: any;
+  manga: any = {
+    id_manga: 1,
+    name_manga: "Shingeky no Kyojin",
+    id_author: 1,
+    cap_manga: 139,
+    genre_manga: "Shounen",
+    debut_manga: "2009-09-09T00:00:00.000Z",
+    cover_manga: "https://mangadex.org/covers/304ceac3-8cdb-4fe7-acf7-2b6ff7a60613/12158456-0511-468b-be37-8d2aa3772723.png",
+    status_manga: "Finished",
+    name_author: "Hajime Isayama"
+  }
+  saved: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  formatDate(date: any) {
+    return moment(date).format('YYYY-MM-DD')
+  }
+
+  toggleButton() {
+    this.saved = !this.saved;
   }
 
 }
