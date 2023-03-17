bf79abc61ee73fb2eb4bd21b8996d003d12c273b
Kevin-CrA <a1117140138@utch.edu.mx>
2023-03-17 14:11:30 -0600

    Sign up page, api connection

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup,FormControl,Validators } from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
+
+
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupPage implements OnInit {
 
-  constructor() { }
+  signUpForm: FormGroup;
 
+
+  constructor(public formSU: FormBuilder, public http: HttpClient) { // <-- Added public http: HttpClient
+    this.signUpForm = this.formSU.group({
+     'username': new FormControl("", Validators.required),
+     'email': new FormControl("", Validators.required),
+     'pass': new FormControl("", Validators.required),
+     
+    });
+  }
+  
   ngOnInit() {
   }
-
+  
+  register(){
+    if (this.signUpForm.invalid) {
+      console.log("Invalid");
+    }
+    else{
+      this.http.post('http://192.168.137.212:3000/register', this.signUpForm.value).subscribe((res) => {
+        console.log("Connected");
+        
+      })
+      console.log(this.signUpForm.value);
+    }
+  }
 }
